48bce44ccd9b461fd6ae46b570cb25edb72634de
goliverma <gcool73@gmail.com>
2020-09-25 11:31:03 +0530

    change launch setting

--- EmployeeManag.Web/Startup.cs
+++ EmployeeManag.Web/Startup.cs
@@ -1,14 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using EmployeeManag.Web.Models;
 using EmployeeManag.Web.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
